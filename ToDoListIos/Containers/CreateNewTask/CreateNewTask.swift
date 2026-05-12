2ec47c0edd315f5d64074f31bc54855dfbda8795
Abdulkareem Mashabi <abdulkareem_mashabi@hotmail.com>
2026-05-12 11:14:20 +0300

    complete implementation ui for createNewTask file

--- ToDoListIos/Containers/CreateNewTask/CreateNewTask.swift
+++ ToDoListIos/Containers/CreateNewTask/CreateNewTask.swift
@@ -8,8 +8,44 @@
 import SwiftUI
 
 struct CreateNewTask: View {
+    @State private var title: String = ""
+    @State private var description: String = ""
+    @State private var isPresented: Bool = true
+    @State private var selectedDate = Date()
+    @EnvironmentObject private var appColors: AppColors
+    private var isButtonDisabled: Bool {
+        return title.isEmpty
+    }
+    @State private var color: ColorsToDo = ColorsToDo.red
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack(alignment: .bottom){
+            Image(color.image).resizable()
+                .scaledToFill().ignoresSafeArea()
+        
+  
+            VStack {
+                TextInput(data: $title, placeholder: "Title" ,error: "Title is required")
+                DateInput(dateIconColor: color.color, placeholder: "Date (Optional)")
+                TextInput(data: $description, placeholder: "Description" ,error: "Description is required", isTextArea: true)
+                
+                ButtonComponent {
+                    Task {
+                        
+                    }
+                } label: {
+                    Text("Submit")
+                }.fontWeight(.bold).foregroundColor(.white)
+                    .frame(maxWidth: .infinity, maxHeight: 40)
+                    .background(color.color)
+                    .cornerRadius(16).disabled(isButtonDisabled).opacity(isButtonDisabled ? 0.5 : 1)
+            }.padding(12).frame(width: UIScreen.main.bounds.width).background(    RoundedRectangle(cornerRadius: 20)
+                .fill(.white)
+                .shadow(radius: 6).ignoresSafeArea()).onAppear {
+                    color = appColors.getImage()
+                }
+        }
+        
     }
 }
 
