467c6d8515ea355511b7ce90f4178e189edb694f
Abdulkareem Mashabi <abdulkareem_mashabi@hotmail.com>
2026-07-18 13:02:12 +0300

    create modifier for button to enhance the usage of the component

--- ToDoListIos/Components/ButtonComponent.swift
+++ ToDoListIos/Components/ButtonComponent.swift
@@ -10,9 +10,11 @@ import Lottie
 
 struct ButtonComponent<LabelView: View>: View {
     var action: () -> Void
-    @ViewBuilder var label: () -> LabelView
+     var label: () -> LabelView
     @EnvironmentObject private var loadingManager: LoadingManager
     let filePath = "/Users/abdulkareemmashabi/Desktop/ToDoListIos/ToDoListIos/Lotties/loadingButton.json"
+    var isButtonForm: Bool = false
+    @State var isButtonDisabled: Bool = false
     
     var body: some View {
         Button(action: action) {
@@ -25,6 +27,30 @@ struct ButtonComponent<LabelView: View>: View {
                 label().frame(maxWidth: .infinity, maxHeight: .infinity)
             }
             
+        }
+    }
+}
+
+extension View {
+    func formButtonStyle() -> some View {
+        self
+            .fontWeight(.bold)
+            .foregroundColor(.white)
+            .frame(maxWidth: .infinity, maxHeight: 40)
+            .background(.cyan)
+            .cornerRadius(16)
+    }
+}
+
+extension View {
+    @ViewBuilder
+    func isButtonDisabled(_ disabled: Bool) -> some View {
+        if disabled {
+            self
+                .disabled(true)
+                .opacity(0.5)
+        } else {
+            self
         }
     }
 }
---
+++ ToDoListIos/Containers/AccountDeletion/AccountDeletion.swift
@@ -0,0 +1,18 @@
+//
+//  AccountDeletion.swift
+//  ToDoListIos
+//
+//  Created by Abdulkareem Mashabi on 03/02/1448 AH.
+//
+
+import SwiftUI
+
+struct AccountDeletion: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AccountDeletion()
+}
