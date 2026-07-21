64371dd6929581a7f14c8cdc288bdf873eeb7619
Abdulkareem Mashabi <abdulkareem_mashabi@hotmail.com>
2026-07-21 12:00:03 +0300

    adding new api for create task

--- ToDoListIos/APIs/taskAPIs.swift
+++ ToDoListIos/APIs/taskAPIs.swift
@@ -4,4 +4,20 @@
 //
 //  Created by Abdulkareem Mashabi on 09/11/1447 AH.
 //
+import FirebaseFirestore
 
+func addTaskAPI (task: ToDoTask) async throws {
+    do {
+        let db = Firestore.firestore()
+        
+        let encoder = Firestore.Encoder()
+        
+        var data: [String: Any] = [:]
+        data["mainTask"] = try encoder.encode(task.mainTask)
+        data["subTasks"] = []
+        
+        try await db.collection(Storage.load(key: "token")!)    .addDocument(data: data)
+    } catch {
+        throw error
+    }
+}
