8facd5e9b226cb916cfa36d82f7d41b1790ac316
Abdulkareem Mashabi <abdulkareem_mashabi@hotmail.com>
2026-07-25 18:43:58 +0300

    create a function to fetch tasks

--- ToDoListIos/APIs/taskAPIs.swift
+++ ToDoListIos/APIs/taskAPIs.swift
@@ -21,3 +21,23 @@ func addTaskAPI (task: ToDoTask) async throws {
         throw error
     }
 }
+
+func fetchAllTasksAPI () async throws -> [ToDoTask] {
+    do {
+        let db = Firestore.firestore()
+        
+        let snapshot = try await db.collection(Storage.load(key: "token")!).getDocuments()
+        for document in snapshot.documents {
+            print(document.documentID)
+            print(document.data())
+        }
+        
+        return try snapshot.documents.map { document in
+            let documentData = try document.data(as: ToDoTask.self)
+            return ToDoTask(documentID: document.documentID, mainTask: documentData.mainTask, subTasks: documentData.subTasks)
+        }
+    }
+    catch {
+        throw error
+    }
+}
