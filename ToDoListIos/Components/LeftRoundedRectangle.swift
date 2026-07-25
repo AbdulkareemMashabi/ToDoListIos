feec5ab9e225c9fec254f4b07cb4866fc59a1f3e
Abdulkareem Mashabi <abdulkareem_mashabi@hotmail.com>
2026-07-25 18:43:09 +0300

    new component for left sided color

---
+++ ToDoListIos/Components/LeftRoundedRectangle.swift
@@ -0,0 +1,37 @@
+//
+//  LeftRoundedRectangle.swift
+//  ToDoListIos
+//
+//  Created by Abdulkareem Mashabi on 11/02/1448 AH.
+//
+
+import SwiftUI
+struct LeftRoundedRectangle: Shape {
+    var radius: CGFloat = 12
+
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+
+        path.move(to: CGPoint(x: rect.maxX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.minX + radius, y: rect.minY))
+        path.addArc(
+            center: CGPoint(x: rect.minX + radius, y: rect.minY + radius),
+            radius: radius,
+            startAngle: .degrees(-90),
+            endAngle: .degrees(-180),
+            clockwise: true
+        )
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY - radius))
+        path.addArc(
+            center: CGPoint(x: rect.minX + radius, y: rect.maxY - radius),
+            radius: radius,
+            startAngle: .degrees(180),
+            endAngle: .degrees(90),
+            clockwise: true
+        )
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.closeSubpath()
+
+        return path
+    }
+}
