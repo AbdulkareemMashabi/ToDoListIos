b8896d41cda827059fbaa7e203132b7170a4cf5f
Abdulkareem Mashabi <abdulkareem_mashabi@hotmail.com>
2026-07-25 18:41:13 +0300

    adding package

---
+++ ToDoListIos/Components/TaskListComponent.swift
@@ -0,0 +1,18 @@
+//
+//  TaskListComponent.swift
+//  ToDoListIos
+//
+//  Created by Abdulkareem Mashabi on 11/02/1448 AH.
+//
+
+import SwiftUI
+
+struct TaskListComponent: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TaskListComponent()
+}
