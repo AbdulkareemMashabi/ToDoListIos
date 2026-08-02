b0a5bc74b89a225bbe619e3e2226fb71c7644585
Abdulkareem Mashabi <abdulkareem_mashabi@hotmail.com>
2026-08-02 17:19:27 +0300

    change model

--- ToDoListIos/Models.swift
+++ ToDoListIos/Models.swift
@@ -19,13 +19,49 @@ struct LoginBody: Codable {
     let userId: String
 }
 
-struct MainTask : Codable {
-    let calendarId: String?
+struct MainTask: Codable {
+    let calendarId: String
     let color: String
-    let date: String?
-    let description: String?
+    let date: String
+    let description: String
     var status: Bool
     let title: String
+
+    enum CodingKeys: String, CodingKey {
+        case calendarId
+        case color
+        case date
+        case description
+        case status
+        case title
+    }
+
+    init(
+        calendarId: String,
+        color: String,
+        date: String,
+        description: String,
+        status: Bool,
+        title: String,
+    ) {
+        self.calendarId = calendarId
+        self.color = color
+        self.date = date
+        self.description = description
+        self.status = status
+        self.title = title
+    }
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+
+        calendarId = try container.decodeIfPresent(String.self, forKey: .calendarId) ?? ""
+        color = try container.decode(String.self, forKey: .color)
+        date = try container.decodeIfPresent(String.self, forKey: .date) ?? ""
+        description = try container.decodeIfPresent(String.self, forKey: .description) ?? ""
+        status = try container.decode(Bool.self, forKey: .status)
+        title = try container.decode(String.self, forKey: .title)
+    }
 }
 
 struct SubTasks: Codable {
@@ -35,6 +71,7 @@ struct SubTasks: Codable {
 
 struct ToDoTask: Codable {
     @DocumentID var documentID: String?
+    var favorite: Bool = false
     var mainTask: MainTask
-    var subTasks: [SubTasks]
+    var subTasks: [SubTasks] = []
 }
