b1bcbfec5317b678dcae1c9c44a9ff22248da485
Abdulkareem Mashabi <abdulkareem_mashabi@hotmail.com>
2026-08-15 11:11:39 +0300

    adding lottie manager

---
+++ ToDoListIos/helperClasses/LottieManager.swift
@@ -0,0 +1,13 @@
+//
+//  LottieManager.swift
+//  ToDoListIos
+//
+//  Created by Abdulkareem Mashabi on 02/03/1448 AH.
+//
+
+import Foundation
+
+@MainActor
+class LottieManager: ObservableObject {
+    @Published var isDoneLottieEnabled = false
+}
